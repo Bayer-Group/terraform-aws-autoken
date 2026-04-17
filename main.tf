e00f70b2784ba3de82b2b4c00e246c3fc17fffd1
Sambit Behera <sambit.behera@bayer.com>
2026-04-17 16:28:40 +0530

    update node version

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ module "lambda" {
   version = "6.4.0"
 
   function_name = "${local.name}"
-  runtime       = "nodejs20.x"
+  runtime       = "nodejs22.x"
   handler       = "lambda.handler"
   source_path   = [
     "${path.module}/functions/dist/lambda.js",
